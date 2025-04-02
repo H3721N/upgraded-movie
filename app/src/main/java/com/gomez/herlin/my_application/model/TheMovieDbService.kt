8819d414da0e74cbc89aab9020c372b66362198c
Herlin Gomez <hgomez@bytesw.com>
2025-04-02 09:44:19 -0600

    Se realiza un listado deacuerdo a la busqueda segun region y se descargan mas librerias

--- app/src/main/java/com/gomez/herlin/my_application/model/TheMovieDbService.kt
+++ app/src/main/java/com/gomez/herlin/my_application/model/TheMovieDbService.kt
@@ -7,5 +7,8 @@ import retrofit2.http.Query
 interface TheMovieDbService {
 
     @GET("movie/popular")
-    suspend fun listPopularMovies(@Query("api_key") apiKey: String): MOvieDbResult
+    suspend fun listPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("region") region: String
+    ): MOvieDbResult
 }
