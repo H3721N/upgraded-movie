11d6d0f8892e265eb11cae65ab85050ce96ff6a6
Herlin Gomez <hgomez@bytesw.com>
2025-04-02 09:35:41 -0600

    Se muestra el titulo de la pelicula seleccionada

--- app/src/main/java/com/gomez/herlin/my_application/ui/main/MovieView.kt
+++ app/src/main/java/com/gomez/herlin/my_application/ui/main/MovieView.kt
@@ -1,10 +1,12 @@
-package com.gomez.herlin.my_application
+package com.gomez.herlin.my_application.ui.main
 
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.gomez.herlin.my_application.R
+import com.gomez.herlin.my_application.model.Movie
 
 class MovieView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -18,5 +20,10 @@ class MovieView @JvmOverloads constructor(
 
         cover = view.findViewById(R.id.coverImg)
         title = view.findViewById(R.id.coverTitle)
+    }
+
+    fun setMovie(movie: Movie) {
+        title.text = movie.title
+        //cover.image = movie.cover
     }
 }
